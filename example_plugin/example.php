b9fb1f3e36341a923e3a775f3ab85833cb766436
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2023-06-25 10:34:02 +0100

    Update example.php

--- example_plugin/example.php
+++ example_plugin/example.php
@@ -1,6 +1,6 @@
 <?php
 require_once "../../inc/common.php";
 require_once "../../inc/header.php";
-<h4> Example Page</h4>
+echo "<h4> Example Page</h4>"
 echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 require_once "../../inc/footer.php";
