bbee452a51473ac4149f3c51c81e96a9664b75b3
Valerie Pond <v.a.pond@outlook.com>
2023-06-27 06:22:26 +0100

    Update mail-settings.php

--- php_mailer/mail-settings.php
+++ php_mailer/mail-settings.php
@@ -10,7 +10,7 @@ if (!current_user_can(PERMISSION_MANAGE_PLUGINS))
 }
 foreach($_POST as $key => $value)
 {
-	var_dump($key,$value);
+	var_dump("duck",$key,$value);
 	if ($key == "fromEmail")
 		$config['smtp']["username"];
 	elseif ($key == "smtpPass")
@@ -24,7 +24,7 @@ foreach($_POST as $key => $value)
 	elseif ($key == "smtpEnc")
 		$config['smtp']['smtpEnc'];
 
-	write_config_file();
+	write_config();
 }
 ?>
 
