5aeddbf9dcb309d921d27a8714d978f0d0845fc9
Madriix <espace.live@yahoo.fr>
2024-08-13 00:37:18 +0200

    Repair of the close button on the popup

--- IPCloneDetectorAndASNAnalyzer/index.php
+++ IPCloneDetectorAndASNAnalyzer/index.php
@@ -779,7 +779,7 @@ function asnExists($asn, $fileContent)
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="customModalLabel">Modal title</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body" id="custom_results">
             </div>
